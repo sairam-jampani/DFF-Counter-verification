1d62b49a17634a66addfe4d94648ed89383c94ae
SNV SATYA SAI RAM <satyasairamjampani@gmail.com>
2026-09-13 13:51:37 +0530

    Create counter.v

---
+++ counter.v
@@ -0,0 +1,16 @@
+
+module counter(
+    input clk,
+    input rst,
+    output reg [3:0] count = 4'b0000
+);
+
+always @(posedge clk)
+begin
+    if(rst)
+        count <= 4'b0000;
+    else
+        count <= count + 1'b1;
+end
+
+endmodule
